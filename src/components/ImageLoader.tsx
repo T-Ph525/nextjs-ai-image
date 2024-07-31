0d29c818444cfc6f0530a0ecfed84365c66df1f5
Evanʎx <95352755+T-Ph525@users.noreply.github.com>
2024-07-31 12:23:26 +1000

    Update ImageLoader.tsx

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -1,7 +1,8 @@
 export default function ImageLoader({ isLoading }: { isLoading: boolean }) {
-    return (
-        <div className={`sm:w-[300px] sm:h-[300px] w-[166px] h-[166px] bg-slate-200 ${isLoading ? "animate-pulse" : ""}`}>
-
-        </div>
-    )
+  return (
+    <div className={`
+      w-full h-auto max-w-full max-h-full 
+      bg-slate-200 ${isLoading ? "animate-pulse" : ""}
+    `} />
+  );
 }
