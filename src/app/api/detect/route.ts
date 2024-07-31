9071d9611b09d9ca8202c36e2a4159e8322c4e8b
Evanʎx <95352755+T-Ph525@users.noreply.github.com>
2024-07-31 12:29:04 +1000

    Update route.ts

--- src/app/api/detect/route.ts
+++ src/app/api/detect/route.ts
@@ -3,16 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
   const { prompt } = await req.json();
 
-  const response = await fetch("https://api-inference.huggingface.co/models/UnfilteredAI/NSFW-gen-v2",
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/UnfilteredAI/NSFW-gen-v2",
     {
       headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
       method: "POST",
-      body: JSON.stringify({ inputs: prompt, width: 300, height: 300}),
-}
+      body: JSON.stringify({ inputs: prompt, width: 300, height: 300 }),
+    }
   );
 
-  const result = await response.json();
-//   console.log(result)
-  
-  return NextResponse.json({ result });
+  const result = await response.arrayBuffer(); // Get response as ArrayBuffer
+
+  // Convert ArrayBuffer to base64 string
+  const base64Image = Buffer.from(result).toString("base64");
+
+  return NextResponse.json({ result: base64Image }); // Return base64 image
 };
